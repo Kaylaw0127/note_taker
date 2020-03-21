8490d6af7fe2701724c1579610054d580e5e4f77
Kayla Ward <kaylaw0127@gmail.com>
2020-03-20 18:57:24 -0700

    home page route added

--- assets/js/server.js
+++ assets/js/server.js
@@ -6,3 +6,17 @@ const notes = require('../../db/db.json');
 const app = express()
 const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static(__dirname + "/public"));
+
+// Route for the home page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../index.html"));
+  });
+
+
+// Runs the server on the specified port
+app.listen(PORT, () => {
+    console.log("APP LISTENING ON PORT " + PORT)
+})
